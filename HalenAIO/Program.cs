8aba8dff33833d2a4459e605595ddc7dcba23449
HalenRepo <tim3025@msn.com>
2017-05-05 18:49:06 +1000

    Commit

--- HalenAIO/Program.cs
+++ HalenAIO/Program.cs
@@ -1,2 +1,13 @@
 //Usings
 
+namespace HalenAIO
+{
+	Class Program
+	{
+		public static string[] ChampList = { "Ezreal" };
+		public static Spell Q, W, E, R;
+		private static string DevLog = "(WIP)";
+		
+		public static Menu Config;
+	}
+}

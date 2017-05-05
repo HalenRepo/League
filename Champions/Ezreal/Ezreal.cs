693e7f12ca1171307fcd2db5d4606face9a9f7ba
HalenRepo <tim3025@msn.com>
2017-05-05 19:47:14 +1000

    Ezreal update

--- Champions/Ezreal/Ezreal.cs
+++ Champions/Ezreal/Ezreal.cs
@@ -15,10 +15,41 @@ namespace HalenAIO.Champions
             W.SetSkillshot(0.25f, 80f, 1600f, false, SkillshotType.SkillshotLine);
 			R.SetSkillshot(1.1f, 160f, 2000f, false, SkillshotType.SkillshotLine);
 			
-			//Menu items for customisation here.
+			//Menu items for customisation here, such as min mana for harass etc.
 			
 			Game.OnUpdate += Game_OnUpdate;
 			Drawing.OnDraw += Drawing_OnDraw;
+		}
+		
+		public Game_OnUpdate(EventArgs args)
+		{
+			if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
+			{
+				Combo();
+			}
+			
+			if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
+			{
+				Harass();
+			}
+			
+			if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear)
+			{
+				LaneClear();
+			}
+		}
+		
+		public static void Combo()
+		{
+			if (Q.IsReady()) //if spell is ready
+			{
+				var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical); //Get target via SDK.
+				
+				if (t.IsValidTarget())
+				{
+					Q.Cast(t); //Cast at target.
+				}
+			}
 		}
 	}
 }

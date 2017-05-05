49916438293d689faae71cf2eebc4c1057a0200f
HalenRepo <tim3025@msn.com>
2017-05-05 19:05:26 +1000

    Commit

--- HalenAIO/Program.cs
+++ HalenAIO/Program.cs
@@ -4,10 +4,38 @@ namespace HalenAIO
 {
 	Class Program
 	{
+		//Supported champions
 		public static string[] ChampList = { "Ezreal" };
 		public static Spell Q, W, E, R;
 		private static string DevLog = "(WIP)";
 		
 		public static Menu Config;
+		
+		public static Obj_AI_Hero Player = ObjectManager.Player;
+		public static Orbwalking.Orbwalker Orbwalker;
+		
+		public static bool LaneClear = false;
+		public static bool Harass = false;
+		public static bool Combo = false;
+		
+		Game.OnUpdate += Game_OnGameUpdate;
+		Drawing.OnDraw += Drawing_OnDraw;
+		
+		private static void Game_OnGameLoad(EventArgs args)
+		{
+			Config = new Menu("HalenAIO", "HalenAIO", true);
+			
+			//Determine champion
+			switch (Player.ChampionName)
+			{
+				case "Ezreal":
+				new Champions.Ezreal();
+				break;
+			}
+			
+			//Use SDK prediction here.
+			
+		}
+		
 	}
 }
